9c142e9e33e83fe426cd7e11b205f9802589b779
mingfang914 <phuongvnmp@gmail.com>
2025-03-06 22:41:07 +0700

    Test

---
+++ WebApp/ITexam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITexam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
